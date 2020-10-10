9a7c09f1703a07e8924b65c15ab7e972ebdee8fa
unknown <saaluissaka>
2020-10-10 12:40:06 +0000

    first commit

---
+++ first.py
@@ -0,0 +1,8 @@
+friends = ['Mustapha', 'Ahmed', 'Dawood']
+for friend in friends:
+    print('Hi ' + friend)
+
+numbers = ['2.5', '3.5', '4.5', '5.5']
+
+for num in numbers:
+    print("Age " + num)
---
+++ variable.py
@@ -0,0 +1,4 @@
+name = 'Saalu'
+x = 1
+y = 2.5
+test = True
